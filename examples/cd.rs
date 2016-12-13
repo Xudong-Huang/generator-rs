47f6a344a537fa68fadc7a096325a8f4abf8aac7
Xudong Huang <huangxu008@hotmail.com>
2016-12-13 17:16:37 +0800

    add more examples

---
+++ examples/cd.rs
@@ -0,0 +1,54 @@
+#[macro_use]
+extern crate generator;
+use generator::*;
+
+#[derive(Debug)]
+enum Action {
+    Play(&'static str),
+    Stop,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum State {
+    Playing,
+    Stopped,
+}
+
+use Action::*;
+use State::*;
+
+fn main() {
+    let mut cd_player = Gn::new_scoped(|mut s| {
+        let mut state = Stopped;
+        loop {
+            match s.get_yield() {
+                Some(Play(s)) => {
+                    if state == Stopped {
+                        println!("I'm playing {}", s);
+                        state = Playing;
+                    } else {
+                        println!("should first stop");
+                    }
+                }
+                Some(Stop) => {
+                    if state == Playing {
+                        println!("I'm stopped");
+                        state = Stopped;
+                    } else {
+                        println!("I'm already stopped");
+                    }
+                }
+                a => {
+                    println!("invalid action: {:?}", a);
+                }
+            }
+            s.yield_with(state);
+        }
+    });
+
+    cd_player.send(Play("hello world"));
+    cd_player.send(Play("hello another day"));
+    cd_player.send(Stop);
+    cd_player.send(Stop);
+    cd_player.send(Play("hello another day"));
+}
---
+++ examples/pipe.rs
@@ -0,0 +1,30 @@
+#![feature(conservative_impl_trait)]
+#[macro_use]
+extern crate generator;
+use generator::*;
+
+fn main() {
+    fn square<'a, T: Iterator<Item = u32> + 'a>(input: T) -> impl Iterator<Item = u32> + 'a {
+        Gn::new_scoped(|mut s| {
+            for i in input {
+                s.yield_with(i * i);
+            }
+            done!();
+        })
+    }
+
+    fn sum<'a, T: Iterator<Item = u32> + 'a>(input: T) -> impl Iterator<Item = u32> + 'a {
+        Gn::new_scoped(|mut s| {
+            let mut acc = 0;
+            for i in input {
+                acc += i;
+                s.yield_with(acc);
+            }
+            done!();
+        })
+    }
+
+    for i in sum(square(0..10)) {
+        println!("i = {:?}", i);
+    }
+}
