c02da81c9015af893940c412e3b624ac1b5e4261
Xudong Huang <huangxu008@hotmail.com>
2018-07-09 09:48:51 +0800

    :pencil: use Vec<T> as default raw buffer allocator

--- src/alloc.rs
+++ src/alloc.rs
@@ -4,17 +4,20 @@ pub struct RawVec<T> {
 }
 
 impl<T> RawVec<T> {
+    #[inline]
     pub fn with_capacity(cap: usize) -> Self {
         RawVec {
             buf: Vec::with_capacity(cap),
         }
     }
 
+    #[inline]
     pub fn ptr(&self) -> *mut T {
         let ptr = self.buf.as_ptr();
         ptr as *mut T
     }
 
+    #[inline]
     pub fn cap(&self) -> usize {
         self.buf.capacity()
     }
